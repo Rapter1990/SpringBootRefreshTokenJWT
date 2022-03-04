e27bc1e58a5f12fcdf96fe83887ba8c363173292
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-03-04 17:39:38 +0300

    Task 15 : Define CustomUserDetails and CustomUserDetailsService for Security process

---
+++ src/main/java/com/refreshtokenjwt/app/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.refreshtokenjwt.app.security;
+
+import com.refreshtokenjwt.app.exception.UserNotFoundException;
+import com.refreshtokenjwt.app.modal.User;
+import com.refreshtokenjwt.app.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User Name " + username + " not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
