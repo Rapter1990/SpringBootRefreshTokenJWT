b39cbff068f7305b221e10730fcf290ba8dc1ed2
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-03-06 00:31:20 +0300

    Task 18 : Add AuthController and PageController

---
+++ src/main/java/com/refreshtokenjwt/app/controller/AuthController.java
@@ -0,0 +1,226 @@
+package com.refreshtokenjwt.app.controller;
+
+import com.refreshtokenjwt.app.exception.RefreshTokenException;
+import com.refreshtokenjwt.app.exception.RoleException;
+import com.refreshtokenjwt.app.jwt.JwtUtils;
+import com.refreshtokenjwt.app.modal.ERole;
+import com.refreshtokenjwt.app.modal.RefreshToken;
+import com.refreshtokenjwt.app.modal.Role;
+import com.refreshtokenjwt.app.modal.User;
+import com.refreshtokenjwt.app.payload.request.LoginRequest;
+import com.refreshtokenjwt.app.payload.request.LogoutRequest;
+import com.refreshtokenjwt.app.payload.request.SignupRequest;
+import com.refreshtokenjwt.app.payload.request.TokenRefreshRequest;
+import com.refreshtokenjwt.app.payload.response.JWTResponse;
+import com.refreshtokenjwt.app.payload.response.MessageResponse;
+import com.refreshtokenjwt.app.payload.response.TokenRefreshResponse;
+import com.refreshtokenjwt.app.security.CustomUserDetails;
+import com.refreshtokenjwt.app.service.impl.RefreshTokenService;
+import com.refreshtokenjwt.app.service.impl.RoleService;
+import com.refreshtokenjwt.app.service.impl.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
+    UserService userService;
+
+    RoleService roleService;
+
+    RefreshTokenService refreshTokenService;
+
+    AuthenticationManager authenticationManager;
+
+    PasswordEncoder encoder;
+
+    JwtUtils jwtUtils;
+
+    @Autowired
+    public AuthController(UserService userService, RoleService roleService, RefreshTokenService refreshTokenService,
+                          AuthenticationManager authenticationManager, PasswordEncoder encoder, JwtUtils jwtUtils) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.refreshTokenService = refreshTokenService;
+        this.authenticationManager = authenticationManager;
+        this.encoder = encoder;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signUpRequest) {
+
+        LOGGER.info("AuthController | registerUser is started");
+
+        String username = signUpRequest.getUsername();
+        String email = signUpRequest.getEmail();
+        String password = signUpRequest.getPassword();
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if(userService.existsByUsername(username)){
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if(userService.existsByEmail(email)){
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already taken!"));
+        }
+
+        User user = new User();
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(encoder.encode(password));
+
+
+        if (strRoles != null) {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "ROLE_ADMIN":
+
+                        Role adminRole = roleService.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RoleException("Error: Admin Role is not found."));
+                        roles.add(adminRole);
+                        break;
+
+                    case "ROLE_MODERATOR":
+
+                        Role moderatorRole = roleService.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RoleException("Error: Moderator Role is not found."));
+                        roles.add(moderatorRole);
+
+                        break;
+
+                    default:
+
+                        Role userRole = roleService.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RoleException("Error: User Role is not found."));
+                        roles.add(userRole);
+
+                }
+
+            });
+        }else{
+
+            Role userRole = new Role();
+            userRole.setName(ERole.ROLE_USER);
+
+            roles.add(userRole);
+        }
+
+        user.setRoles(roles);
+        userService.saveUser(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+
+        LOGGER.info("AuthController | authenticateUser is started");
+
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+
+        LOGGER.info("AuthController | authenticateUser | username : " + username);
+        LOGGER.info("AuthController | authenticateUser | password : " + password);
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username,password);
+
+        LOGGER.info("AuthController | authenticateUser | usernamePasswordAuthenticationToken : " + usernamePasswordAuthenticationToken.toString());
+
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        String jwt = jwtUtils.generateJwtToken(userDetails);
+
+        LOGGER.info("AuthController | authenticateUser | jwt : " + jwt);
+
+        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        LOGGER.info("AuthController | authenticateUser | roles : " + roles.toString());
+
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
+
+        LOGGER.info("AuthController | authenticateUser | refreshToken : " + refreshToken.toString());
+
+        JWTResponse jwtResponse = new JWTResponse();
+        jwtResponse.setEmail(userDetails.getEmail());
+        jwtResponse.setUsername(userDetails.getUsername());
+        jwtResponse.setId(userDetails.getId());
+        jwtResponse.setToken(jwt);
+        jwtResponse.setRefreshToken(refreshToken.getToken());
+        jwtResponse.setRoles(roles);
+
+        LOGGER.info("AuthController | authenticateUser | jwtResponse : " + jwtResponse.toString());
+
+        return ResponseEntity.ok(jwtResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logoutUser(@RequestBody LogoutRequest logoutRequest) {
+
+        LOGGER.info("AuthController | logoutUser is started");
+
+        int userId = logoutRequest.getUserId();
+
+        int deletedValue = refreshTokenService.deleteByUserId(userId);
+
+        if(deletedValue == 1){
+            return ResponseEntity.ok(new MessageResponse("Log out successful!"));
+        }else{
+            return ResponseEntity.ok(new MessageResponse("You're already logout"));
+        }
+
+    }
+
+    @PostMapping("/refreshtoken")
+    public ResponseEntity<?> refreshtoken(@RequestBody TokenRefreshRequest request) {
+
+        LOGGER.info("AuthController | refreshtoken is started");
+
+        String requestRefreshToken = request.getRefreshToken();
+
+        LOGGER.info("AuthController | refreshtoken | requestRefreshToken : " + requestRefreshToken);
+
+        RefreshToken token = refreshTokenService.findByToken(requestRefreshToken)
+                .orElseThrow(() -> new RefreshTokenException(requestRefreshToken + "Refresh token is not in database!"));
+
+        LOGGER.info("AuthController | refreshtoken | token : " + token.toString());
+
+        RefreshToken deletedToken = refreshTokenService.verifyExpiration(token);
+
+        LOGGER.info("AuthController | refreshtoken | deletedToken : " + deletedToken.toString());
+
+        User userRefreshToken = deletedToken.getUser();
+
+        LOGGER.info("AuthController | refreshtoken | userRefreshToken : " + userRefreshToken.toString());
+
+        String newToken = jwtUtils.generateTokenFromUsername(userRefreshToken.getUsername());
+
+        LOGGER.info("AuthController | refreshtoken | newToken : " + newToken);
+
+        return ResponseEntity.ok(new TokenRefreshResponse(newToken, requestRefreshToken));
+
+    }
+}
---
+++ src/main/java/com/refreshtokenjwt/app/controller/PageController.java
@@ -0,0 +1,35 @@
+package com.refreshtokenjwt.app.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pages")
+public class PageController {
+
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Content.";
+    }
+
+    @GetMapping("/moderator")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public String moderatorAccess() {
+        return "Moderator Content.";
+    }
+
+}
